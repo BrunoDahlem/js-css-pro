e47b4406ff0c6a3eadff53da60a55393788126b7
Reweb-Bruno <60668141+Reweb-Bruno@users.noreply.github.com>
2021-06-10 22:15:57 -0300

    Aula012 JSON + JS

---
+++ aula012/objetos/js/main.js
@@ -0,0 +1,9 @@
+// var data = new Date();
+// var mes = data.getMonth();
+// console.log(mes); utilizando class Primitiva
+
+var produto = {};
+produto.nome = 'Monitor';
+produto.preco = 1234.56;
+produto.tamanho = [32,40,42,50];
+console.log(produto);
